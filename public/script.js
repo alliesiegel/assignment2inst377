5cba9e2b9ea80a2ebbd95f888dbc7a142a653734
alegonz320 <70660097+alegonz320@users.noreply.github.com>
2020-11-08 16:27:13 -0500

    js_eventListener

--- public/script.js
+++ public/script.js
@@ -1,4 +1,25 @@
-function filterByRestaurants(){
-    const restaurants = document.getElementsByClassName('textinput');
+function filterByRestaurantName(){
+    const filterText = document.getElementsByClassName('textinput');
+
     
 }
+function filterByCategory(){
+    const filterText = document.getElementsByClassName('textinput');
+}
+
+document.body.addEventListener('input', async (e) => {
+    e.preventDefault(); // this stops whatever the browser wanted to do itself.
+    const form = $(e.target).serializeArray();
+    fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
+    })
+      .then((fromServer) => fromServer.json())
+      .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
+      .catch((err) => {
+        console.log(err);
+      });
+  });
