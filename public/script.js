31f087affd52db6ccc01a75368a20069d42e0328
Allison Siegel <asiegel1@umd.edu>
2020-11-08 16:23:42 -0500

    things from the wes bos tutorial

--- public/script.js
+++ public/script.js
@@ -2,3 +2,36 @@ function filterByRestaurants(){
     const restaurants = document.getElementsByClassName('textinput');
     
 }
+const endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
+const stuff = [];
+
+fetch (endpoint)
+.then(blob => blob.json())
+.then(data => stuff.push(...data))
+
+function findMatches(wordtoMatch, stuff)
+{ return stuff.filter(resta =>{
+    const regex = new RegExp(wordtoMatch, 'gi');
+    return resta.category.match(regex) || resta.name.match(regex)
+});
+
+}
+function displayMatches() {
+    const matchArray = findMatches(this.value, stuff);
+    const html = matchArray.map(resta => {
+        return `
+        <li>
+            <span class = "name">${resta.category}, ${resta.name}</span>
+            <span class ="address">${resta.city}, ${resta.state},${resta.zip},${resta.address_line_1}</span>
+        </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+
